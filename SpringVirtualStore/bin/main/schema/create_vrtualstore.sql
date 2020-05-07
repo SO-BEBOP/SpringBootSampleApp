afe530ac11fc7354b4f386186d9069b5ac5b452e
ShotaOkamura-Bebop <bebop.creative.lab@gmail.com>
2020-05-07 09:11:14 +0900

    Updated schema.

--- SpringVirtualStore/bin/main/schema/create_vrtualstore.sql
+++ SpringVirtualStore/bin/main/schema/create_vrtualstore.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: vrtualstore
+-- Host: 127.0.0.1    Database: virtualstore
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
@@ -25,14 +25,13 @@ DROP TABLE IF EXISTS `business_tbl`;
 CREATE TABLE `business_tbl` (
   `business_id` int NOT NULL AUTO_INCREMENT,
   `business_user_id` int NOT NULL DEFAULT '0',
-  `business_product_id` int NOT NULL DEFAULT '0',
   `business_sales` int DEFAULT '1',
   `business_state` int DEFAULT '1',
   `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updata_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`business_id`,`business_user_id`,`business_product_id`),
+  PRIMARY KEY (`business_id`,`business_user_id`),
   UNIQUE KEY `business_id_UNIQUE` (`business_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,8 +40,36 @@ CREATE TABLE `business_tbl` (
 
 LOCK TABLES `business_tbl` WRITE;
 /*!40000 ALTER TABLE `business_tbl` DISABLE KEYS */;
-INSERT INTO `business_tbl` VALUES (0,0,0,1000,1,'2020-04-22 22:47:13','2020-04-22 22:47:56'),(1,0,0,700,1,'2020-04-23 02:37:09','2020-04-23 02:37:46'),(2,0,0,2000,1,'2020-04-23 02:37:46','2020-04-23 02:37:46');
+INSERT INTO `business_tbl` VALUES (1,1,700,1,'2020-05-03 14:52:15','2020-05-03 14:53:56'),(2,1,1000,1,'2020-05-03 14:55:59','2020-05-05 17:33:44'),(3,0,18000,1,'2020-05-06 11:06:52','2020-05-06 11:06:52');
 /*!40000 ALTER TABLE `business_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart_tbl`
+--
+
+DROP TABLE IF EXISTS `cart_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart_tbl` (
+  `cart_id` int NOT NULL AUTO_INCREMENT,
+  `cart_user_id` int NOT NULL DEFAULT '0',
+  `cart_product_id` int NOT NULL DEFAULT '0',
+  `cart_state` int NOT NULL DEFAULT '1',
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updata_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cart_id`),
+  UNIQUE KEY `cart_id_UNIQUE` (`cart_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart_tbl`
+--
+
+LOCK TABLES `cart_tbl` WRITE;
+/*!40000 ALTER TABLE `cart_tbl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart_tbl` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,12 +85,11 @@ CREATE TABLE `product_mst` (
   `product_price` int DEFAULT '0',
   `product_stock` int DEFAULT '0',
   `product_state` int DEFAULT '1',
-  `final_sales` date DEFAULT NULL,
   `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updata_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`product_id`),
   UNIQUE KEY `product_id_UNIQUE` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +98,7 @@ CREATE TABLE `product_mst` (
 
 LOCK TABLES `product_mst` WRITE;
 /*!40000 ALTER TABLE `product_mst` DISABLE KEYS */;
-INSERT INTO `product_mst` VALUES (0,'SampleProduct_1',1000,200,1,NULL,'2020-04-14 12:24:28','2020-04-23 02:38:57'),(1,'SampleProduct_2',700,100,1,NULL,'2020-04-23 02:38:57','2020-04-23 02:38:57');
+INSERT INTO `product_mst` VALUES (0,'SampleProduct_1',3000,1000,1,'2020-04-14 12:24:28','2020-05-04 19:59:45'),(1,'SampleProduct_2',5000,500,1,'2020-04-23 02:38:57','2020-05-04 19:59:45'),(2,'SampleProduct_3',10000,100,1,'2020-05-04 11:24:20','2020-05-04 19:59:45');
 /*!40000 ALTER TABLE `product_mst` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +114,7 @@ CREATE TABLE `user_mst` (
   `user_name` varchar(10) NOT NULL DEFAULT 'No name.',
   `user_password` varchar(255) NOT NULL DEFAULT 'password',
   `user_birthday` date DEFAULT NULL,
-  `user_gender` varchar(45) DEFAULT NULL,
+  `user_gender` varchar(45) DEFAULT 'unselected',
   `user_state` int DEFAULT '1',
   `user_role` varchar(20) DEFAULT 'ROLE_GENERAL',
   `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -96,7 +122,7 @@ CREATE TABLE `user_mst` (
   PRIMARY KEY (`user_id`,`user_name`),
   UNIQUE KEY `user_num_UNIQUE` (`user_id`),
   UNIQUE KEY `user_name_UNIQUE` (`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,9 +131,17 @@ CREATE TABLE `user_mst` (
 
 LOCK TABLES `user_mst` WRITE;
 /*!40000 ALTER TABLE `user_mst` DISABLE KEYS */;
-INSERT INTO `user_mst` VALUES (0,'admin','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa','1900-01-01','Not set.',1,'ROLE_ADMIN','2020-04-14 10:33:11','2020-04-19 13:25:43'),(1,'general','$2a$10$2jkFyXaS97QQ/IcX2IKkpOnSXwVNR7D1p6Mcw4ALQ3foz0D9d4Tl.','1989-06-03','male',1,'ROLE_GENERAL','2020-04-21 13:22:34','2020-04-23 02:40:49');
+INSERT INTO `user_mst` VALUES (0,'admin','$2a$10$rFXsCbWx8vTzTdJuiuN3lOeTrmECJ76lEE2NEy9JwEvNPbp6AB5nG','1900-01-01','unselected',1,'ROLE_ADMIN','2020-04-14 10:33:11','2020-05-04 19:58:32'),(1,'general','$2a$10$9v5PiKuXlROCgABlNxTJmOpOw7LiSoNoN9Q7OalaClT4lEc7LNdQS','1989-06-03','male',1,'ROLE_GENERAL','2020-04-21 13:22:34','2020-05-04 19:58:32');
 /*!40000 ALTER TABLE `user_mst` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'virtualstore'
+--
+
+--
+-- Dumping routines for database 'virtualstore'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -118,4 +152,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-24  9:47:56
+-- Dump completed on 2020-05-07  9:09:14
